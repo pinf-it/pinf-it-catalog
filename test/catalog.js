38c9763d39266b598c770cb5c58f6d5a8d3a4fe5
Christoph Dorn <christoph@christophdorn.com>
2013-10-20 22:21:08 -0700

    initial commit

---
+++ test/catalog.js
@@ -0,0 +1,13 @@
+
+const PATH = require("path");
+const ASSERT = require("assert");
+const CATALOG = require("../lib/catalog");
+
+//const MODE = "test";
+const MODE = "write";
+
+const DEBUG = false;
+
+describe('catalog', function() {
+
+});
